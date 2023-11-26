61807d057b4a841dde84774021dcf4c2b1dc0080
Fatmazadjali <maathermohammed77@gmail.com>
2023-11-26 15:48:23 +0400

    class practical

---
+++ practicals solution/practicals/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Globalization;
+
+namespace practicals
+{
+	 class Program
+	{
+		static void Multi(int i )
+		{
+			
+			
+			for (i = 1; i < 7; i++ )
+			{
+				for (int j = 1; j <= 12; j++)
+					
+					Console.WriteLine($" {i} * {j} = {i * j} ");
+				
+			}
+			
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			Multi(4);
+
+
+		}
+	}
+}
